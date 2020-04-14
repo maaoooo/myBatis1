1a77ab023d7447f57b6cf97c8c452571ae39ee0e
maaoooo <1224337540@qq.com>
2020-04-14 18:01:46 +0800

    updata

--- src/main/java/com/demogorgen/HelloMybatis.java
+++ src/main/java/com/demogorgen/HelloMybatis.java
@@ -1,11 +1,14 @@
 package com.demogorgen;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author lzr
@@ -14,9 +17,8 @@ import java.io.InputStream;
  */
 public class HelloMybatis {
     public static void main(String[] args) throws IOException {
-        test();
+        test3();
     }
-
     public static void test() throws IOException {
         //读取配置文件
         InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
@@ -28,8 +30,37 @@ public class HelloMybatis {
         SqlSession session = ssf.openSession();
         //操作数据库
         User user=session.selectOne("UserMapper.SelectUserByID",1);
-        System.out.println(user.getUsername());
-
+        System.out.println(user);
+    }
+    public static void test2() throws IOException {
+        //读取配置文件
+        InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
+        //需要sqlsessionFactoryBulider
+        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+        //创建sqlsessionfactory
+        SqlSessionFactory ssf = ssfb.build(in);
+        //生产sqlsession
+        SqlSession session = ssf.openSession();
+        //操作数据库
+        List<User> userList=session.selectList("UserMapper.SelectAllUser");
+        for(User u:userList){
+            System.out.println(u);
+        }
+    }
+    public static void test3() throws IOException {
+        //读取配置文件
+        InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
+        //需要sqlsessionFactoryBulider
+        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+        //创建sqlsessionfactory
+        SqlSessionFactory ssf = ssfb.build(in);
+        //生产sqlsession
+        SqlSession session = ssf.openSession();
+        //操作数据库
+        List<User> userList=session.selectList("UserMapper.SelectAllLike","刘");
+        for(User u:userList){
+            System.out.println(u);
+        }
     }
 
 }
