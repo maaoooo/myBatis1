d9d9a74459c8c1d525794093f72ba4a178fc7e97
maaoooo <1224337540@qq.com>
2020-04-17 17:57:47 +0800

    update

---
+++ src/main/java/com/demogorgen/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.demogorgen.mapper;
+
+import com.demogorgen.User;
+
+import java.util.List;
+
+/**
+ * @author lzr
+ * @date 2020/4/17 10:47:17
+ * @description
+ */
+public interface UserMapper {
+    //mapper动态代理开发四大原则
+    //1.接口方法名必须与mapper。xml要调用的sql语句id一致
+    //接口的形参类型需要与mapper.xml parameterType一致
+    //接口的返回值需要与mapper.xml resultType一致
+    //mapper.xml 中namespace要与接口的全包名一致
+    //注意mapper动态代理开发中，根据返回值类型来自动选择
+
+    //通过id查询一个用户
+    public User SelectUserByID(int id);
+    public List<User> SelectAllUser();
+    public List<User> SelectAllLike(String name);
+}
---
+++ src/main/java/com/demogorgen/test/MapperTest.java
@@ -0,0 +1,69 @@
+package com.demogorgen.test;
+
+import com.demogorgen.User;
+import com.demogorgen.mapper.UserMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author lzr
+ * @date 2020/4/17 10:55:35
+ * @description
+ */
+public class MapperTest {
+    public static void main(String[] args) {
+        MapperTest mapperTest=new MapperTest();
+        mapperTest.Test3();
+    }
+    public void Test1(){
+        //读取配置文件
+        InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
+        //需要sqlsessionFactoryBulider
+        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+        //创建sqlsessionfactory
+        SqlSessionFactory ssf = ssfb.build(in);
+        //生产sqlsession
+        SqlSession session = ssf.openSession();
+        //操作数据库
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        User user=mapper.SelectUserByID(2);
+        System.out.println(user);
+    }
+    public void Test2(){
+        //读取配置文件
+        InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
+        //需要sqlsessionFactoryBulider
+        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+        //创建sqlsessionfactory
+        SqlSessionFactory ssf = ssfb.build(in);
+        //生产sqlsession
+        SqlSession session = ssf.openSession();
+        //操作数据库
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        List<User> userList=mapper.SelectAllUser();
+        for(User u:userList){
+            System.out.println(u);
+        }
+    }
+    public void Test3(){
+        //读取配置文件
+        InputStream in = HelloMybatis.class.getClassLoader().getResourceAsStream("conf/sqlMapConfig.xml");
+        //需要sqlsessionFactoryBulider
+        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
+        //创建sqlsessionfactory
+        SqlSessionFactory ssf = ssfb.build(in);
+        //生产sqlsession
+        SqlSession session = ssf.openSession();
+        //操作数据库
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        List<User> userList=mapper.SelectAllLike("克");
+        for(User u:userList){
+            System.out.println(u);
+        }
+    }
+
+}
